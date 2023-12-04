b7dd8fa01d65ab1768ccba927a9df66be09fcc94
zsb258 <zhuang.shengbin@u.nus.edu>
2023-12-04 16:14:26 +0800

    5 CPU Exceptions

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,14 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     // panic!("Some panic message");
 
+    blog_os::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
+
+    println!("It did not crash!");
 
     loop {}
 }
