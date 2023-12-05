a347ce9593a9661aa60af1d020659de3bbb3a303
zsb258 <zhuang.shengbin@u.nus.edu>
2023-12-05 12:59:23 +0800

    6 Double Fault Exceptions

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,19 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    // x86_64::instructions::interrupts::int3();
+    //
+    // // trigger a page fault
+    // unsafe {
+    //     *(0xdeadbeef as *mut u8) = 42;
+    // };
+
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+
+    // trigger a stack overflow
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
