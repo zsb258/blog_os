8b5494fd4953842889dbabb326bb937295ac59e7
zsb258 <zhuang.shengbin@u.nus.edu>
2023-12-07 12:13:39 +0800

    9 Paging Implementation

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@
 
 use core::panic::PanicInfo;
 
+use bootloader::{entry_point, BootInfo};
+
 use blog_os::println;
+
+entry_point!(kernel_main);
 
 /// This function is called on panic.
 #[cfg(not(test))]
@@ -22,8 +26,7 @@ fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
     // panic!("Some panic message");
 
@@ -66,6 +69,43 @@ pub extern "C" fn _start() -> ! {
             "Level 4 page table at: {:?}",
             level_4_page_table.start_address()
         );
+    }
+
+    {
+        use blog_os::memory;
+        use x86_64::structures::paging::{Page, Translate};
+        use x86_64::VirtAddr;
+
+        let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+        let mut mapper = unsafe { memory::init(phys_mem_offset) };
+
+        let addresses = [
+            // the identity-mapped vga buffer page
+            0xb8000,
+            // some code page
+            0x201008,
+            // some stack page
+            0x0100_0020_1a10,
+            // virtual address mapped to physical address 0
+            boot_info.physical_memory_offset,
+        ];
+
+        for &address in &addresses {
+            let virt = VirtAddr::new(address);
+            let phys = mapper.translate_addr(virt);
+            println!("{:?} -> {:?}", virt, phys);
+        }
+
+        let mut frame_allocator =
+            unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+        // map a page to VGA buffer frame
+        let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+        memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+        // write the string `New!` to the screen through the new mapping
+        let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+        unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
     }
 
     #[cfg(test)]
